8781f915da5826ba56580b88712ed44b7b643178
Johnson Chen <chnejohnson@gmail.com>
2024-07-15 12:24:23 +0800

    failed to execute order

---
+++ zerodev/9_execute_order.ts
@@ -0,0 +1,97 @@
+import { createKernelAccount, createKernelAccountClient, createZeroDevPaymasterClient } from '@zerodev/sdk'
+import { KERNEL_V3_1 } from '@zerodev/sdk/constants'
+import { signerToEcdsaValidator } from '@zerodev/ecdsa-validator'
+import { http, createPublicClient, zeroAddress, encodeFunctionData, erc20Abi } from 'viem'
+import { privateKeyToAccount } from 'viem/accounts'
+import { sepolia } from 'viem/chains'
+import { ENTRYPOINT_ADDRESS_V07, bundlerActions } from 'permissionless'
+import 'dotenv/config'
+import { getExecuteScheduledTransferAction } from '@rhinestone/module-sdk'
+
+const MTK_ADDRESS = '0x2bb2F59B2F316e1Fd68616b83920A1fe15E32a81'
+const recipient = '0xd78B5013757Ea4A7841811eF770711e6248dC282' // dev
+const startDate = Math.floor(Date.now() / 1000) + 60 // UNIX timestamp
+const executeInterval = 60 // 1 minute
+const numberOfExecutions = 2
+
+const PROJECT_ID = process.env.ZERODEV_PROJECT_ID as string
+const PRIVATE_KEY = process.env.PRIVATE_KEY as string
+
+if (!PROJECT_ID || !PRIVATE_KEY) {
+	throw new Error('Please set your project id and private key in a .env file')
+}
+
+const BUNDLER_RPC = `https://rpc.zerodev.app/api/v2/bundler/${PROJECT_ID}`
+const PAYMASTER_RPC = `https://rpc.zerodev.app/api/v2/paymaster/${PROJECT_ID}`
+
+const chain = sepolia
+const entryPoint = ENTRYPOINT_ADDRESS_V07
+const kernelVersion = KERNEL_V3_1
+
+const main = async () => {
+	const signer = privateKeyToAccount(`0x${PRIVATE_KEY}`)
+
+	const publicClient = createPublicClient({
+		transport: http(BUNDLER_RPC),
+	})
+
+	const ecdsaValidator = await signerToEcdsaValidator(publicClient, {
+		signer,
+		entryPoint,
+		kernelVersion,
+	})
+
+	const account = await createKernelAccount(publicClient, {
+		plugins: {
+			sudo: ecdsaValidator,
+		},
+		entryPoint,
+		kernelVersion,
+	})
+
+	const kernelClient = createKernelAccountClient({
+		account,
+		chain,
+		entryPoint,
+		bundlerTransport: http(BUNDLER_RPC),
+		middleware: {
+			sponsorUserOperation: async ({ userOperation }) => {
+				const zerodevPaymaster = createZeroDevPaymasterClient({
+					chain,
+					entryPoint,
+					transport: http(PAYMASTER_RPC),
+				})
+				return zerodevPaymaster.sponsorUserOperation({
+					userOperation,
+					entryPoint,
+				})
+			},
+		},
+	})
+
+	const accountAddress = kernelClient.account.address
+	console.log('My account:', accountAddress)
+
+	const action = getExecuteScheduledTransferAction({
+		jobId: 1,
+	})
+
+	const userOpHash = await kernelClient.sendUserOperation({
+		userOperation: {
+			callData: action.callData,
+		},
+	})
+
+	console.log('UserOp hash:', userOpHash)
+	console.log('Waiting for UserOp to complete...')
+
+	const bundlerClient = kernelClient.extend(bundlerActions(entryPoint))
+	await bundlerClient.waitForUserOperationReceipt({
+		hash: userOpHash,
+		timeout: 0,
+	})
+
+	console.log('View completed UserOp here: https://jiffyscan.xyz/userOpHash/' + userOpHash)
+}
+
+main()
